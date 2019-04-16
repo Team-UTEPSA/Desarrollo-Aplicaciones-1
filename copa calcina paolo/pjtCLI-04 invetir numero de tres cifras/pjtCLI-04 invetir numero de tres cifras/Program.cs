5fed0be06cb74ca34e53d088b1d59ff9a0149aa5
Paolocop <49662439+Paolocop@users.noreply.github.com>
2019-04-15 21:01:54 -0400

    proyecot tres cifras
    practicas

---
+++ copa calcina paolo/pjtCLI-04 invetir numero de tres cifras/pjtCLI-04 invetir numero de tres cifras/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjtCLI_04_invetir_numero_de_tres_cifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, dec, inv, cen, uni;
+            string entrada;
+            Console.Write("INVERTIR NUMERO DE TRES DIGITOS");
+            Console.WriteLine();
+            Console.WriteLine("INGRESE TRES NUMEROS");
+            entrada = Console.ReadLine();
+            num = int.Parse(entrada);
+            cen = num / 100;
+            num = num % 100;
+            dec = num / 10;
+            uni = num % 10;
+            inv = (uni * 100) + (dec * 10) + cen;
+            Console.WriteLine();
+            Console.WriteLine("NUMERO INVERTIDO ES " + inv);
+            Console.WriteLine();
+            Console.WriteLine("pulse cualquier tecla para salir");
+            Console.ReadLine();
+            
+        }
+    }
+}
