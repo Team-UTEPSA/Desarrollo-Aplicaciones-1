76ebb0cbb2fdadc21d0d4689e716c830ecdf16d9
Paolocop <49662439+Paolocop@users.noreply.github.com>
2019-04-15 21:20:47 -0400

    2do proyecto
    segundo proyecto a subir

---
+++ copa calcina paolo/pjtCLI-05 OPERACIONES BASICAS/pjtCLI-05 OPERACIONES BASICAS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjtCLI_05_OPERACIONES_BASICAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b,c,D;
+            
+            string entrada;
+            Console.Write("INSERTE EL PRIMER NUMERO ");
+            entrada = Console.ReadLine();
+            a = int.Parse(entrada);
+
+            Console.Write("INSERTE EL SEGUNDO NUMERO ");
+            entrada = Console.ReadLine();
+            b = int.Parse(entrada);
+
+            Console.WriteLine();
+            c = a + b;
+            Console.WriteLine("LA SUMA ES= "+c);
+
+            c = a - b;
+            Console.WriteLine("LA RESTA ES= " + c);
+
+            c = a * b;
+            Console.WriteLine("LA MULTIPLICACION ES= " + c);
+
+            D = a / b;
+            Console.WriteLine("LA DIVICION ES= " +D);
+
+            c = a % b;
+            Console.WriteLine("EL RESIDUO ES= " + c);
+            Console.WriteLine("PRECIONE UNA TECLA");
+            Console.ReadLine();
+
+        }
+    }
+}
