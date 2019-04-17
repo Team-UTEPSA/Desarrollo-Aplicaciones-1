094d1faacf73ecd0da09eded2a8c32deefac7b2a
Beymar Jiménez Ruiz <beymarjimenez@gmail.com>
2019-04-17 18:44:07 -0400

    Práctica 0203
    Publicación

---
+++ Jimenez Ruiz Beymar/pjtCLI_0203 Invertir un numero de dos cifras/pjtCLI_03 Invertir un numero de dos cifras/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjtCLI_03_Invertir_un_numero_de_dos_cifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, inv, dec, uni;
+            string entrada;
+
+            Console.WriteLine("INVERTIR UN NÚMERO DE DOS DÍGITOS");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine();
+            Console.Write("Escriba un número de dos dígitos: ");
+            entrada = Console.ReadLine();
+            num = int.Parse(entrada);
+            dec = num / 10;
+            uni = num % 10;
+            inv = (uni * 10) + dec;
+            Console.WriteLine();
+            Console.WriteLine("El número " + num.ToString() + " invertido es " + inv.ToString());
+            Console.WriteLine();
+            Console.Write("Pulse una tecla para salir... ");
+            Console.ReadKey();
+        }
+    }
+}
