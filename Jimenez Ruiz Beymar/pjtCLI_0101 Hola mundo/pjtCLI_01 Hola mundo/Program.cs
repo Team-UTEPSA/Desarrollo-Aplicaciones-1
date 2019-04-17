52d3e26912be19ad8f07a4504a341d8699248281
Beymar Jiménez Ruiz <beymarjimenez@gmail.com>
2019-04-17 18:45:23 -0400

    Práctica 0101
    Publicación

---
+++ Jimenez Ruiz Beymar/pjtCLI_0101 Hola mundo/pjtCLI_01 Hola mundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjtCLI_01_Hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo...");
+            Console.WriteLine("");
+            Console.Write("Pulse una tecla para salir: ");
+            Console.ReadKey();
+        }
+
+    }
+}
