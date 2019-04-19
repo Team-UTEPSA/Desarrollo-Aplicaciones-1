027b594aa334bfafbd0e1cae22af6d62c61dcb69
deiver10 <49662668+deiver10@users.noreply.github.com>
2019-04-18 21:19:34 -0400

    1parcial

---
+++ Campos Montano Deiver/1Parcial/pjt_Pregunta 5/pjt_Pregunta 5/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjt_Pregunta_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examen = new List<double>();
+            int ce;
+           
+            ce = LeerInt("Escriba un número en el rango [", 1, 100);
+           // ce = LeerInt("Escriba un número para la lista");
+
+            //Lista Examen
+            examen = CargarRandom(examen, ce, 1, 120);
+
+            examen.Sort(); 
+
+            int e = examen.BinarySearch(0);
+
+            Console.WriteLine("EXAMEN");
+            Console.WriteLine("----");
+            Listar(examen);
+
+        
+
+            Console.WriteLine("-------------------");
+           
+           
+
+            //Para Salir
+            Console.WriteLine();
+            Console.Write("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+
+        static public int LeerInt(string msg, int vi, int vf)
+        {
+            string entrada;
+            int n = 0;
+            bool esNumero = false;
+
+            try
+            {
+                do
+                {
+                    Console.Write(msg + vi.ToString() + " ; " + vf.ToString() + "]: ");
+                    entrada = Console.ReadLine();
+                    esNumero = int.TryParse(entrada, out n);
+                } while (n < vi || n > vf || !esNumero);
+            }
+            catch
+            {
+                Console.WriteLine("Ingrese un número en el rango [1 ; 100]");
+            }
+
+            return n;
+        }
+
+
+        static public List<int> CargarRandom(List<int> l, int ne, int vi, int vf)
+        {
+            Random randomVentas = new Random();
+            double nr = 0;
+            int i = 1;
+
+            while (i <= ne)
+            {
+                nr = randomVentas.Next(vi, vf);
+                l.Add(nr);
+                i += 1;
+            }
+
+            return l;
+        }
+
+
+        static public void Listar(List<int> l)
+        {
+            
+            double n = l.Count();
+            double iEle = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                iEle = l[i];
+                Console.WriteLine(iEle.ToString());
+            }
+        }
+    }
+    }
+
---
+++ Campos Montano Deiver/1Parcial/pjt_Pregunta 6/pjt_Pregunta 6/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjt_Pregunta_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            List<double> Examen = new List<double>();
+            int ce;
+
+            ce = LeerDouble("Escriba un número para su lista");
+
+            //Lista Edad
+            Examen = CargarRandom(Examen, ce, 1, 120);
+
+            Examen.Sort();  
+
+            int e = Examen.BinarySearch(0);
+
+            Console.WriteLine("Examen");
+            Console.WriteLine("----");
+            Listar(Examen);
+
+            //Estadistica
+            Console.WriteLine("ESTADÍSTICA");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Suma total: " + suma(Examen).ToString());
+            Console.WriteLine("Promedio: " + promedio(Examen).ToString());
+            Console.WriteLine("Máximo: " + max(Examen).ToString());
+            Console.WriteLine("Mínimo: " + min(Examen).ToString());
+            Console.WriteLine("---------");
+    
+
+            //Salida
+            Console.WriteLine();
+            Console.Write("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+
+        static public int LeerDouble(string msg, int vi, int vf)
+        {
+            string entrada;
+            int n = 0;
+            bool esNumero = false;
+
+            try
+            {
+                do
+                {
+                    Console.Write(msg + vi.ToString() + " ; " + vf.ToString() + "]: ");
+                    entrada = Console.ReadLine();
+                    esNumero = int.TryParse(entrada, out n);
+                } while (n < vi || n > vf || !esNumero);
+            }
+            catch
+            {
+                Console.WriteLine("ERROR:: Ingrese un nuevo número");
+            }
+
+            return n;
+        }
+
+
+        static public List<double> CargarRandom(List<double> l, int ne, int vi, int vf)
+        {
+            Random randomVentas = new Random();
+            int nr = 0;
+            int i = 1;
+
+            while (i <= ne)
+            {
+                nr = randomVentas.Next(vi, vf);
+                l.Add(nr);
+                i += 1;
+            }
+
+            return l;
+        }
+
+
+        static public void Listar(List<double> l)
+        {
+            //Listar los elementos de la lista
+            int n = l.Count();
+            int abc = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                abc = l[i];
+                Console.WriteLine(abc.ToString());
+            }
+        }
+
+
+        //ESTADÍSTICA
+        static public int suma(List<double> l)
+        {
+            int s = 0;
+            int n = l.Count();
+
+            for (int i = 0; i < n; i++)
+            {
+               s += l[i];
+            }
+
+            return s;
+        }
+
+        static public float promedio(List<double> l)
+        {
+            int n = l.Count();
+            float p = 0;
+
+            p = suma(l) / n;
+
+            return p;
+        }
+
+        static public int min(List<double> l)
+        {
+            l = l.OrderBy(i => i).ToList();  //Ascendentemente
+
+            return l[0];
+        }
+
+
+        static public int max(List<double> l)
+        {
+            l = l.OrderByDescending(i => i).ToList();  //descendente
+
+            return l[0];
+        }
+    }
+
+
+
+        }
+    
+
