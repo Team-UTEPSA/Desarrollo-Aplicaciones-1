2da4ce9e3827fcd2c37a5c7d5e72001d91f04e72
mitzyortizz <mitzyortizzabala@gmail.com>
2019-05-02 22:12:10 -0400

    Población de Tablas SACD
    Población de las tablas "tblPersona", "tblCliente", "tblPersonal"

---
+++ Mitzy Ortiz Zabala/Poblacion de tablas SACD/Scrip_InsertTablas.sql
@@ -0,0 +1,112 @@
+USE dbCancha;
+
+--Poblacion de "tblPersona"
+INSERT INTO tblPersona VALUES (1,'Alberto', 'García Méndez', 1, '1984/06/17', '6102926', 'alberto_garcía17', '52164SDFR');
+INSERT INTO tblPersona VALUES (2,'Alejandro', 'González Cruz', 1, '1945/01/31', '6263008', 'alejandro_gonzález31', '8249753GA');
+INSERT INTO tblPersona VALUES (3,'Alfredo', 'Rodríguez Calvo', 1, '1970/11/30', '9040983', 'alfredo_rodríguez30', 'HFB524789');
+INSERT INTO tblPersona VALUES (4,'Alicia', 'Fernández Gallego', 0, '1978/07/03', '7776043', 'alicia_fernández03', 'UT4579645');
+INSERT INTO tblPersona VALUES (5,'Antonio', 'López Vidal', 1, '1940/03/13', '9085930', 'antonio_lópez13', 'MOZ86715U');
+INSERT INTO tblPersona VALUES (6,'Arturo', 'Martínez León', 1, '1976/04/03', '6178071', 'arturo_martínez', 'OMY145KJ');
+INSERT INTO tblPersona VALUES (7,'Carlos', 'Sánchez Márquez', 1, '1974/06/06', '6544917', 'carlos_sánchez', 'PUH157JE9');
+INSERT INTO tblPersona VALUES (8,'Daniel', 'Pérez Herrera', 1, '1996/07/24', '6484013', 'daniel_pérez', 'KLR467O9L');
+INSERT INTO tblPersona VALUES (9,'David',' Gómez Peña', 1, '1956/05/12', '6250783', 'david_gómez12', 'HRQP82LU9');
+INSERT INTO tblPersona VALUES (10,'Eduardo', 'Martin Flores', 1, '1952/11/08', '7521813', 'eduardo_martin08', 'YHE15LJU9');
+INSERT INTO tblPersona VALUES (11,'Enrique', 'Jiménez Cabrera', 1, '1988/08/15', '9105811', 'enrique_jiménez15', 'TWQN74196');
+INSERT INTO tblPersona VALUES (12,'Fernando', 'Ruiz Campos', 1, '1957/08/05', '5321083', 'fernando_ruiz05', '875394LOK');
+INSERT INTO tblPersona VALUES (13,'Francisca','Hernández Vega', 0, '1977/04/22', '7726358', 'francisca_hernández22', 'WCRESP191');
+INSERT INTO tblPersona VALUES (14,'Francisco', 'Diaz Fuentes', 1, '1952/06/30', '5599008', 'francisco_diaz30', 'Q1G5YF6D7');
+INSERT INTO tblPersona VALUES (15,'Francisco Javier', 'Moreno Carrasco', 1, '1995/11/26', '4244887', 'francisco_javier26', 'OP45ED38N');
+INSERT INTO tblPersona VALUES (16,'Gerardo', 'Muñoz Diez', 1, '1960/11/17', '3593624', 'gerardo_muñoz17', 'MKIPLS291');
+INSERT INTO tblPersona VALUES (17,'Guadalupe', 'Álvarez Caballero', 0, '1976/08/08', '5967836', 'guadalupe_álvarez08', 'QWEASDZXC');
+INSERT INTO tblPersona VALUES (18,'Javier', 'Romero Reyes', 1, '1953/07/06', '9277077', 'javier_romero06', 'PLY943Q12');
+INSERT INTO tblPersona VALUES (19,'Jesús', 'Alonso Nieto', 1, '1966/10/26', '5157454', 'jesús_alonso26', 'DFGTRE843');
+INSERT INTO tblPersona VALUES (20,'Jorge', 'Gutiérrez Aguilar', 1, '1988/01/15', '6718725', 'jorge_gutiérrez15', 'HHT7YUZZ8');
+INSERT INTO tblPersona VALUES (21,'José', 'Navarro Pascual', 1, '1949/11/29', '5578524', 'josé_navarro29', 'MFO548730');
+INSERT INTO tblPersona VALUES (22,'José Antonio', 'Torres Santana', 1, '1999/08/22', '8049319', 'antonio_torres22', '000QQKL89');
+INSERT INTO tblPersona VALUES (23,'José Luis', 'Domínguez Herrero', 1, '1957/08/04', '8443051', 'luis_domínguez04', 'KII54831M');
+INSERT INTO tblPersona VALUES (24,'José Manuel', 'Vázquez Lorenzo', 1, '1978/11/19', '7276211', 'manuel_vázquez19', 'XYOG4FS0Q');
+INSERT INTO tblPersona VALUES (25,'Josefina', 'Ramos Montero', 0, '1993/07/06', '5574775', 'josefina_ramos06', 'QDRL0FU0E');
+INSERT INTO tblPersona VALUES (26,'Juan', 'Gil Hidalgo', 1, '1980/12/14', '4601271', 'juan_gil14', 'LKJCDE072');
+INSERT INTO tblPersona VALUES (27,'Juan Carlos', 'Ramírez Giménez', 1, '1982/02/18', '7445018', 'carlos_ramírez18', '9461SD082');
+INSERT INTO tblPersona VALUES (28,'Juana', 'Serrano Ibáñez', 0, '1961/04/18', '3317382', 'juana_serrano18', '981BF5Y0H');
+INSERT INTO tblPersona VALUES (29,'Leticia', 'Blanco Ferrer', 0, '1986/12/15', '4398609', 'leticia_blanco15', 'WIQL24F0H');
+INSERT INTO tblPersona VALUES (30,'Luis', 'Molina Duran', 1, '1973/04/01', '9776912', 'luis_molina01', 'QIGL051F0');
+INSERT INTO tblPersona VALUES (31,'Manuel', 'Morales Santiago', 1, '1967/03/20', '6314180', 'manuel_morales20', '0501BFTW9');
+INSERT INTO tblPersona VALUES (32,'Marco Antonio', 'Suarez Benítez', 1, '1979/03/05', '9468030', 'marco_suarez05', '852DMSU85');
+INSERT INTO tblPersona VALUES (33,'Margarita', 'Ortega Mora', 0, '1999/01/20', '5365512', 'margarita_ortega20', 'C10D8E3T5');
+INSERT INTO tblPersona VALUES (34,'María', 'Delgado Vicente', 0, '1986/02/09', '7420737', 'maría_delgado09', 'QIE157G03');
+INSERT INTO tblPersona VALUES (35,'María Del Carmen', 'Castro Vargas', 0, '1971/06/09', '9129022', 'mcarmen_castro09', '7853QWZX4');
+INSERT INTO tblPersona VALUES (36,'María Elena', 'Ortiz Arias', 0, '1968/12/10', '3042751', 'elena_ortiz10', '02140QWER');
+INSERT INTO tblPersona VALUES (37,'María Fernanda', 'Rubio Carmona', 0, '1943/03/08', '7422516', 'fernanda_rubio08', '13AXMIR54');
+INSERT INTO tblPersona VALUES (38,'María Guadalupe', 'Marín Crespo', 0, '1971/06/14', '6461178', 'guadalupe_marín14', 'MIW182687');
+INSERT INTO tblPersona VALUES (39,'Mario', 'Sanz Román', 1, '1973/04/23', '6815614', 'mario_sanz23', 'YF5Y2N0W3');
+INSERT INTO tblPersona VALUES (40,'Martín', 'Núñez Pastor', 1, '1954/04/29', '4784485', 'martín_núñez29', 'MOPS9Q25E');
+INSERT INTO tblPersona VALUES (41,'Miguel', 'Iglesias Soto', 1, '1970/10/04', '3469308', 'miguel_iglesias04', 'S5599WQ06');
+INSERT INTO tblPersona VALUES (42,'Miguel Ángel', 'Medina Sáez', 1, '1970/11/17', '4312651', 'ángel_medina17', '45615QWER');
+INSERT INTO tblPersona VALUES (43,'Pedro', 'Garrido Velasco', 1, '1984/06/17', '8073688', 'pedro_garrido17', '258MNBU92');
+INSERT INTO tblPersona VALUES (44,'Rafael', 'Cortes Moya', 1, '1990/05/28', '5082737', 'rafael_cortes28', 'JHY194MAP');
+INSERT INTO tblPersona VALUES (45,'Raúl', 'Castillo Soler', 1, '1981/11/15', '6955834', 'raúl_castillo15', '369BG14J7');
+INSERT INTO tblPersona VALUES (46,'Ricardo', 'Santos Parra', 1, '1964/12/10', '6064428', 'ricardo_santos10', '9147OJRQM');
+INSERT INTO tblPersona VALUES (47,'Roberto', 'Lozano Esteban', 1, '1966/09/19', '5484770', 'roberto_lozano19', 'MDIT24U8O');
+INSERT INTO tblPersona VALUES (48,'Rosa', 'Guerrero Bravo', 0, '1982/06/27', '8795387', 'rosa_guerrero27', '24G5ER0PY');
+INSERT INTO tblPersona VALUES (49,'Sergio', 'Cano Gallardo', 1, '1967/05/01', '9524492', 'sergio_cano01', 'FGWRSCT25');
+INSERT INTO tblPersona VALUES (50,'Teresa', 'Prieto Rojas', 0, '1976/04/03', '6099117', 'teresa_prieto03', 'FWEQPKLT0');
+
+--Poblacion de "tblCliente"
+INSERT INTO tblCliente VALUES (1, 1, '34895147', 'ACEROS HERMAN S.R.L.', 570);
+INSERT INTO tblCliente VALUES (2, 2, '89745214', 'ACRICOLOR ARTE S.R.L.	', 201);
+INSERT INTO tblCliente VALUES (3, 3, '35748914', 'ALTEC LTDA.', 451);
+INSERT INTO tblCliente VALUES (4, 4, '87134975', 'AGRIPAC BOL CIA LTDA.', 570);
+INSERT INTO tblCliente VALUES (5, 5, '32197453', 'AGROINCO S.R.L.', 25);
+INSERT INTO tblCliente VALUES (6, 6, '85479614', 'AUTOBRAS S.A.', 986);
+INSERT INTO tblCliente VALUES (7, 7, '17846924', 'CARPINTERIA JC', 65);
+INSERT INTO tblCliente VALUES (8, 8, '10210475', 'COBOCE LTDA.', 94);
+INSERT INTO tblCliente VALUES (9, 9, '35874156', 'REIMAX S.A.', 123);
+INSERT INTO tblCliente VALUES (10, 10, '10248631', 'AUTOBRAS S.A.', 586);
+INSERT INTO tblCliente VALUES (11, 11, '18314709', 'BATEBOL LTDA.', 415);
+INSERT INTO tblCliente VALUES (12, 12, '30124975', 'COBOL S.A.', 87);
+INSERT INTO tblCliente VALUES (13, 13, '92540490', 'ATIMEX BOLIVIA S.R.L.', 330);
+INSERT INTO tblCliente VALUES (14, 14, '97548347', 'SCHNEIDER ELECTRIC BOLIVIA', 569);
+INSERT INTO tblCliente VALUES (15, 15, '32471098', 'MECOR BOLIVIA S.R.L.', 67);
+INSERT INTO tblCliente VALUES (16, 16, '98724816', 'PRH BOLIVIA', 89);
+INSERT INTO tblCliente VALUES (17, 17, '50149575', 'CONFIPETROL S.A. ', 24);
+INSERT INTO tblCliente VALUES (18, 18, '12048962', 'REPSOL BOLIVIA S.A.', 15);
+INSERT INTO tblCliente VALUES (19, 19, '56408157', 'KIMBERLY CLARK BOLIVIA S.A.', 10);
+INSERT INTO tblCliente VALUES (20, 20, '31795460', 'MONNET', 5);
+INSERT INTO tblCliente VALUES (21, 21, '20146832', 'QWERTY BOLIVIA', 694);
+INSERT INTO tblCliente VALUES (22, 22, '79154730', 'IBC BOLIVIA COURIER', 25);
+INSERT INTO tblCliente VALUES (23, 23, '10327951', 'DUALBIZ S.R.L.', 27);
+INSERT INTO tblCliente VALUES (24, 24, '10347946', 'TEAM DECOR S.R.L', 54);
+INSERT INTO tblCliente VALUES (25, 25, '53108073', 'LA PAPELERA S.A. PACKAGING', 950);
+
+--Poblacion de "tblPersonal"
+INSERT INTO tblPersonal VALUES (1, 26, 'Recepcionista', 0, 'Av. Mutualista, C/Los Cocos');
+INSERT INTO tblPersonal VALUES (2, 27, 'Cobrador', 0, 'Av. Virgen de Lujan, B/Tarechis');
+INSERT INTO tblPersonal VALUES (3, 28, 'Ejecutiva Comercial', 0, 'Av. Cristo Redentor C/Enrique Diez');
+INSERT INTO tblPersonal VALUES (4, 29, 'Guardia', 1, 'Av. Paragua, C/Los Cusis');
+INSERT INTO tblPersonal VALUES (5, 30, 'Ejecutivo Comercial', 0, 'Av. Tres Pasos Al Frente, C/Macororó');
+INSERT INTO tblPersonal VALUES (6, 31, 'Ejecutivo Comercial', 0, 'Av. Beni, B/Hamacas');
+INSERT INTO tblPersonal VALUES (7, 32, 'Administrador', 0, 'Av. San Martín, C/Las Ramplas');
+INSERT INTO tblPersonal VALUES (8, 33, 'Jefe de Marketing', 0, 'Av. Santos Dumont, B/Totaí');
+INSERT INTO tblPersonal VALUES (9, 34, 'Ejecutiva de Marketing', 0, 'Av. Virgen de Cotoca, C/7');
+INSERT INTO tblPersonal VALUES (10, 35, 'Ejecutiva de Marketing', 0, 'Av. Brasil, C/Felix Romero');
+INSERT INTO tblPersonal VALUES (11, 36, 'Supervisora', 0, 'Av. Alemana, C/Las Cutas');
+INSERT INTO tblPersonal VALUES (12, 37, 'Limpieza', 1, 'Av. Pedro Rivera, B/Cupesi');
+INSERT INTO tblPersonal VALUES (13, 38, 'Encargado de Logística', 0, 'Av. Radial 27, P/13');
+INSERT INTO tblPersonal VALUES (14, 39, 'Cajero', 0, 'Av. Roque Aguilera, B/Urbarí');
+INSERT INTO tblPersonal VALUES (15, 40, 'Cajero', 1, 'Av. Piraí, Z/Tres Cruces');
+INSERT INTO tblPersonal VALUES (16, 41, 'Arbitro', 0, 'Av. 18 de Marzo, P/9');
+INSERT INTO tblPersonal VALUES (17, 42, 'Arbitro', 0, 'Av. 24 de Septiembre, B/LAB');
+INSERT INTO tblPersonal VALUES (18, 43, 'Arbitro', 1, 'Av. Virgen de Lujan, B/Retoño');
+INSERT INTO tblPersonal VALUES (19, 44, 'Arbitro', 1, '8vo Anillo, B/Virgen de Guadalupe');
+INSERT INTO tblPersonal VALUES (20, 45, 'Limpieza', 0, 'Av. Noel Kempff, B/Equipetrol');
+INSERT INTO tblPersonal VALUES (21, 46, 'Jardinero', 0, 'Av. Argentina, B/Los Choferes');
+INSERT INTO tblPersonal VALUES (22, 47, 'Organizador de Eventos', 0, 'Av. Adolfo Romero Román, B/Polanco');
+INSERT INTO tblPersonal VALUES (23, 48, 'Organizadora de Eventos', 1, 'Av. Hernando Sanabria, P/Barbery');
+INSERT INTO tblPersonal VALUES (24, 49, 'Entrenador', 0, 'Av. Bush , P/Muralto');
+INSERT INTO tblPersonal VALUES (25, 50, 'Entrenadora', 1, 'Av. Radial 26 , P/Los Guapos');
+
+--Consultando las tablas
+SELECT * FROM tblPersona
+SELECT * FROM tblCliente
+SELECT * FROM tblPersonal
