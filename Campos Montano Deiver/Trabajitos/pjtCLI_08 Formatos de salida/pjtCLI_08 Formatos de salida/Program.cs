3a0d58507e665a1d8738cba707ff03c3f7d4e2b0
deiver10 <49662668+deiver10@users.noreply.github.com>
2019-05-06 20:43:06 -0400

    Trabajos

---
+++ Campos Montano Deiver/Trabajitos/pjtCLI_08 Formatos de salida/pjtCLI_08 Formatos de salida/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjtCLI_08_Formatos_de_salida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BASE, ALTURA, RESUL;
+            string linea;
+            Console.Write("DIGITE LA BASE :");
+            linea = Console.ReadLine();
+            BASE = double.Parse(linea);
+            Console.Write("DIGITE LA ALTURA:");
+            linea = Console.ReadLine();
+            ALTURA = double.Parse(linea);
+            RESUL = (BASE * ALTURA) / 2;
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:####.00}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:c}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:f}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:g}", RESUL));
+            Console.WriteLine(); Console.WriteLine("HOY ES: " + String.Format("Hoy es {0:F}", DateTime.Now));
+            Console.WriteLine("HOY ES: " + String.Format("Hoy es {0:dddd}{0:dd/MM/yyy}", DateTime.Now));
+            Console.Write("Pulse una Tecla:");
+            Console.ReadLine();
+        }
+    }
+}
