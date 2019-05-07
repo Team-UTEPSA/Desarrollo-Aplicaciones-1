338d61b5d7953939f757ef68306be890aa284b55
deiver10 <49662668+deiver10@users.noreply.github.com>
2019-05-06 21:27:52 -0400

    Script

---
+++ Campos Montano Deiver/scriptRelleno.sql
@@ -0,0 +1,23 @@
+
+
+INSERT INTO tblCancha VALUES (1, '1', '101', 'La Morita', 'Futbol 7', 14, '50 mts.', '30 mts.', '150 Bs.','200 Bs.','');
+INSERT INTO tblCancha VALUES (2, '1', '102', 'La Bombonera', 'Futbol 7', 14, '55 mts.', '33 mts.', '190 BS.','220 Bs.','');
+INSERT INTO tblCancha VALUES (3, '1', '103', 'Los Saipineños', 'Futbol 7', 14, '60 mts.', '40 mts', '200 Bs.','250 Bs.','');
+INSERT INTO tblCancha VALUES (4, '2', '104', 'Tutumaso', 'Futbol 11', 22, '120 mts.', '70 mts.', '300 Bs','350 Bs.','');
+INSERT INTO tblCancha VALUES (5, '2', '105', 'Maracana', 'Futbol 11', 22, '110 mts.', '65 mts.', '280 Bs.','320 Bs.','');
+INSERT INTO tblCancha VALUES (6, '2', '106', 'Alemania ', 'Futbol 12', 22, '111 mts.', '66 mts.', '281 Bs.','321 Bs.','');
+INSERT INTO tblCancha VALUES (7, '3', '107', 'Patria', 'Futsal ', 10, '40 mts.', '25 mts.', '150 Bs.','200 Bs.','');
+INSERT INTO tblCancha VALUES (8, '3', '108', 'La Doble Via', 'Futsal ', 10, '35 mts.', '22 mts.', '150 Bs.','180 Bs.','');
+INSERT INTO tblCancha VALUES (9, '3', '109', 'La Doble Via', 'Futbol 7', 14, '50 mts.', '30 mts.', '180 Bs.','230 Bs.','');
+INSERT INTO tblCancha VALUES (10, '4', '110', 'Km 6', 'Futbol 11', 22, '110 mts.', '65 mts.', '290 Bs','330 Bs.','');
+
+
+
+SELECT * FROM tblCancha
+
+
+INSERT INTO tblReserva VALUES (1, '1', '2019/01/10', '10:00:00', '12:00:00', 2);
+
+
+
+SELECT * FROM tblReserva
