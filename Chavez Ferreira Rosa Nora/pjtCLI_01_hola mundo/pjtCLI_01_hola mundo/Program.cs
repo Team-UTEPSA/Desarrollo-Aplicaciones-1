da72833d73fcb58126f3c907e3ffaf8890d1feef
rosachavez <49662441+rosachavez@users.noreply.github.com>
2019-05-07 16:02:19 -0400

    pjt_001
    practicas

---
+++ Chavez Ferreira Rosa Nora/pjtCLI_01_hola mundo/pjtCLI_01_hola mundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjtCLI_01_hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo...");
+            Console.WriteLine("");
+            Console.Write("pulse una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
